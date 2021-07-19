16fad2587c28638d6e6a2aed7c20c4e6ab5aecf0
PufferOverflow <puffer@puffer.blog>
2021-07-20 07:31:32 +0800

    fix

--- utils/config.py
+++ utils/config.py
@@ -5,21 +5,23 @@ from json import load, loads
 
 logger = log.get_logger(name = 'Config')
 
+proxy_url = None
+database_path = None
+telegram_api_secret = None
 if (path.exists('conf/config.ini')):
     config = ConfigParser()
     config.read('conf/config.ini')
-    proxy_url = config.get('Bot', 'proxy_url', fallback=None)
-    database_path = config.get('Bot', 'database_path', fallback=None)
-    telegram_api_secret = config.get('Telegram', 'telegram_api_secret', fallback=None)
-
+    proxy_url = config.get('Bot', 'proxy_url')
+    database_path = config.get('Bot', 'database_path')
+    telegram_api_secret = config.get('Telegram', 'telegram_api_secret')
 proxy_url = environ.get('HTTPS_PROXY') or proxy_url
 database_path = environ.get('DATABASE_PATH') or database_path
 telegram_api_secret = environ.get('TELEGRAM_API_SECRET') or telegram_api_secret
 
+word_list = None
 if (path.exists('conf/keyword_list.json')):
     with open("conf/keyword_list.json", "r") as fp:
         word_list = load(fp)
-
 word_list = loads(environ.get('WORD_LIST_JSON')) if environ.get('WORD_LIST_JSON') else word_list
 
 assert telegram_api_secret, "Telegram API secret not found."
