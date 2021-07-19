84914898a105819277b7bf866977fbc166f9155b
PufferOverflow <puffer@puffer.blog>
2021-07-20 07:14:29 +0800

    fix keyword import

--- utils/config.py
+++ utils/config.py
@@ -16,10 +16,10 @@ proxy_url = environ.get('HTTPS_PROXY') or proxy_url
 database_path = environ.get('DATABASE_PATH') or database_path
 telegram_api_secret = environ.get('TELEGRAM_API_SECRET') or telegram_api_secret
 
-if (path.exists('conf/config.ini')):
-    with open("conf/config.ini", "r") as fp:
+if (path.exists('conf/keyword_list.json')):
+    with open("conf/keyword_list.json", "r") as fp:
         word_list = load(fp)
 
-word_list = loads(environ.get('TELEGRAM_API_SECRET')) if environ.get('TELEGRAM_API_SECRET') else word_list
+word_list = loads(environ.get('WORD_LIST_JSON')) if environ.get('WORD_LIST_JSON') else word_list
 
 assert telegram_api_secret, "Telegram API secret not found."
