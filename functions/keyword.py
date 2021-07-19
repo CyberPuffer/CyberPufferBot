88ccb8174e2ab5d9cdacc6d35abf3588cff52174
PufferOverflow <puffer@puffer.blog>
2021-07-20 07:08:18 +0800

    add env support

--- functions/keyword.py
+++ functions/keyword.py
@@ -1,25 +1,21 @@
-from utils import log
-from json import load
+from utils import log, config
 
 logger = log.get_logger(name = 'Keyword')
-
-with open("conf/keyword_list.json", "r") as fp:
-    word_list = load(fp)
 
 class reply:
     def __init__(self, message) -> None:
         self.type = None
-        for word in word_list:
+        for word in config.word_list:
             if word in message:
                 self.match = word
-                self.type = word_list[word]['type']
+                self.type = config.word_list[word]['type']
                 if self.type == "forward":
-                    self.from_chat_id = word_list[word]['from_chat_id']
-                    self.message_id = word_list[word]['message_id']
+                    self.from_chat_id = config.word_list[word]['from_chat_id']
+                    self.message_id = config.word_list[word]['message_id']
                 elif self.type == "plaintext":
-                    self.text = word_list[word]['text']
+                    self.text = config.word_list[word]['text']
                 elif self.type == "sticker":
-                    self.sticker_id = word_list[word]['sticker_id']
+                    self.sticker_id = config.word_list[word]['sticker_id']
                 else:
                     pass
                 break
--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,7 @@
-from os import path
+from os import path, environ
 from configparser import ConfigParser
 from utils import log
+from json import load, loads
 
 logger = log.get_logger(name = 'Config')
 
@@ -10,6 +11,15 @@ if (path.exists('conf/config.ini')):
     proxy_url = config.get('Bot', 'proxy_url', fallback='')
     database_path = config.get('Bot', 'database_path')
     telegram_api_secret = config.get('Telegram', 'telegram_api_secret')
-else:
-    logger.error('Config file not found.')
-    exit(1)
+
+proxy_url = environ.get('HTTPS_PROXY') or proxy_url
+database_path = environ.get('DATABASE_PATH') or database_path
+telegram_api_secret = environ.get('TELEGRAM_API_SECRET') or telegram_api_secret
+
+if (path.exists('conf/config.ini')):
+    with open("conf/config.ini", "r") as fp:
+        word_list = load(fp)
+
+word_list = loads(environ.get('TELEGRAM_API_SECRET')) if environ.get('TELEGRAM_API_SECRET') else word_list
+
+assert telegram_api_secret, "Telegram API secret not found."
