1facb13c5805635d7ee0684b0c7f86f1ae4cc496
PufferOverflow <puffer@puffer.blog>
2021-08-07 17:04:10 +0800

    add environ for new configs

--- utils/config.py
+++ utils/config.py
@@ -15,11 +15,15 @@ if (path.exists('conf/config.ini')):
     proxy_url = config.get('Bot', 'proxy_url')
     database_path = config.get('Bot', 'database_path')
     telegram_api_secret = config.get('Telegram', 'telegram_api_secret')
-    enabled_functions = config.get('Bot', 'enabled_functions').split(' ')
-    auto_delete_timer = int(config.get('Bot', 'auto_delete_timer'))
+    enabled_functions = config.get('Bot', 'enabled_functions')
+    auto_delete_timer = config.get('Bot', 'auto_delete_timer')
 proxy_url = environ.get('HTTPS_PROXY') or proxy_url
 database_path = environ.get('DATABASE_PATH') or database_path
 telegram_api_secret = environ.get('TELEGRAM_API_SECRET') or telegram_api_secret
+enabled_functions = environ.get('ENABLED_FUNCTIONS') or enabled_functions
+enabled_functions = enabled_functions.split(' ')
+auto_delete_timer = environ.get('AUTO_DELETE_TIMER') or auto_delete_timer
+auto_delete_timer = int(auto_delete_timer)
 
 word_list = None
 if (path.exists('conf/keyword_list.json')):
